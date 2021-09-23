17aeeed809bc057c11ec1540f65cc34c76f8c5e9
Xantrl <mountngubinh@gmail.com>
2021-09-23 15:38:57 +0700

    d

--- mips1.asm
+++ mips1.asm
@@ -3,7 +3,7 @@
     prompt2:    .asciiz      "Enter the second number: "
     menu:      .asciiz      "Enter the number associated with the operation you want performed: 1 => add, 2 => subtract or 3 => multiply: "
     resultText:    .asciiz      "Your final result is: "
-    demotext:     .asciiz     "dsad"
+    demotext:     .asciiz     "dsd"
  .text
 .globl main
 main:
